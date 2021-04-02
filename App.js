b1d6bb5f7fcb28a4535de00a7afb99d80c3a17bb
Karishma <mehar.karishma16@gmail.com>
2021-04-01 23:42:42 -0500

    front end changes

--- App.js
+++ App.js
@@ -73,7 +73,7 @@ const DrawerOptions = () => {
 }
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer independent={true}>
       <DrawerOptions />
     </NavigationContainer>
   );
